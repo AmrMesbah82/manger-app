c773bce6e3bfa6593d4b718650136dbd65f2afbd
Amr Mesbah <amrmesbah33@gmail.com>
2026-09-18 19:07:40 +0000

    Change app ID to com.amrmesbah.manger_plus (Apple: com.amrmesbah.mangerPlus)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.manger_plus"
+    namespace = "com.amrmesbah.manger_plus"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -19,7 +19,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.manger_plus"
+        applicationId = "com.amrmesbah.manger_plus"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         // Firebase Auth needs API 23+.
